e6e33037ab3097985b1c89d823ab373b35fb18b4
Mikael Brockman <mikael@brockman.se>
2016-09-18 23:50:32 +0300

    add default.nix

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ pkgs, stdenv, lib, ... }:
+
+let deps = [pkgs.perl pkgs.curl]; in
+
+stdenv.mkDerivation rec {
+  name = "wd-${version}";
+  version = "0.1";
+  meta = {
+    description = "Command-line tool for WebDriver API";
+    homepage = https://github.com/mbrock/wd;
+    license = lib.licenses.mit;
+  };
+
+  src = ./.;
+  buildInputs = [pkgs.makeWrapper];
+  buildPhase = "true";
+  installPhase =
+    let path = lib.makeBinPath deps; in ''
+      mkdir -p $out/bin && cp $src/wd $out/bin;
+      wrapProgram "$out/bin/wd" --prefix PATH : "${path}"
+    '';
+}
